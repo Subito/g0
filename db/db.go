6c4f7a5d3d438ec76d3a5d37ac15ba42214674b3
Marduk <dr.marduk@gmail.com>
2014-05-06 00:17:04 +0200

    First commit for new table structure.

--- db/db.go
+++ db/db.go
@@ -7,25 +7,12 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"os"
 	"strconv"
-	"time"
 )
 
 type Db struct {
 	DbFile       string
 	DbImageTable string
 	conn         *sql.DB
-}
-
-type Image struct {
-	Id        int
-	Hash      string
-	Name      string
-	Thumbnail string
-	Timestamp time.Time
-	Url       string
-	Network   string
-	Channel   string
-	User      string
 }
 
 func NewDb(DbFile string) (*Db, error) {
---
+++ db/tblImage.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"errors"
+	_ "fmt"
+	"time"
+)
+
+type Image struct {
+
+	// public Fields
+	Id        int
+	Hash      string
+	Name      string
+	Thumbnail string
+	Timestamp time.Time
+	Url       string
+	Network   string
+	Channel   string
+	User      string
+
+	// Config
+	table string
+	db    Db
+}
+
+func Image() *Image {
+	var result *Image = &Image{}
+
+	result.db = NewDb("g0.db")
+}
+
+// Fetch($PicId) returns one Image-struct
+func (i *Image) Fetch(id int) (Image, error) {
+	image, err := i.FetchLatest(id, 1)
+	if err != nil {
+		return Image{}, err
+	}
+	return image[0], nil
+}
+
+// FetchLatest($id, $n) returns n Image-structs, start at $id
+// and n previous db-entries ordered by timestamp desc.
+func (i *Image) FetchLatest(id, n int) ([]Image, error) {
+
+}
